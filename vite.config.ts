15109a553035dce832649a20cb5dd514904c223b
JuegosWeb <gd6qs6gqv9@privaterelay.appleid.com>
2025-10-24 14:20:50 +0200

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,33 @@ import path from 'path';
 export default defineConfig({
   base: '/Portfolio/', // Cambia "portfolio" por el nombre exacto de tu repositorio si es distinto
   plugins: [react()],
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg', 'favicon.ico', 
+        'robots.txt', 'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'Portfolio',
+        short_name: 'Portfolio',
+        description: 'Portfolio de Nahuel Granollers',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   server: {
     port: 3000,
     host: '0.0.0.0',
