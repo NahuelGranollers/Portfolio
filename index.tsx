36acfaaa0a0d25b2cf1498b1acd658bc4a139f53
JuegosWeb <gd6qs6gqv9@privaterelay.appleid.com>
2025-11-22 00:33:29 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -6,28 +6,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { registerSW } from 'virtual:pwa-register';
 
-// Prevent Framer Motion initialization errors with React 19
-const originalObjectDefineProperty = Object.defineProperty;
-Object.defineProperty = function(obj, prop, descriptor) {
-  if (!obj) {
-    console.warn(`Attempted to define property '${String(prop)}' on undefined object`);
-    return obj;
-  }
-  return originalObjectDefineProperty.call(this, obj, prop, descriptor);
-};
-
 registerSW();
 
-// Wait for DOM to be ready
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', initApp);
-} else {
-  initApp();
-}
-
-function initApp() {
-  const rootElement = document.getElementById('root');
-  if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(<App />);
-  }
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
